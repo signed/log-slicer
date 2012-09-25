a1e16e2777c8fb4a95c0451ae1e0653bd2a6672e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-09-25 19:14:25 +0200

    White space change

--- src/test/java/com/github/signed/log/LogEntry_Test.java
+++ src/test/java/com/github/signed/log/LogEntry_Test.java
@@ -12,6 +12,5 @@ public class LogEntry_Test {
     public void retrieveTheTimeStampFromTheLog() throws Exception {
         LogEntry entry = new LogEntry("2012-09-18 20:14:58,518 stuff");
         assertThat(entry.taken(), is(new DateTime(2012, 9,18, 20,14, 58, 518)));
-
     }
 }
