22a2b8b1f873ca27e80514a983a9e7eae4ea6e61
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-10-04 21:31:09 +0200

    All the log parts are extracted and displayed....

--- src/main/java/com/github/signed/log/core/parser/LogEntryParser.java
+++ src/main/java/com/github/signed/log/core/parser/LogEntryParser.java
@@ -6,6 +6,7 @@ import com.github.signed.log.core.LogPart;
 import com.github.signed.log.core.StringLogPart;
 import com.github.signed.log.extractors.loglevel.LogLevelExtractor;
 import com.github.signed.log.extractors.loglocation.LogLocationExtractor;
+import com.github.signed.log.extractors.logmessage.LogMessageExtractor;
 import com.github.signed.log.extractors.thread.LoggedThreadExtractor;
 import com.github.signed.log.extractors.timestamp.TimeStampExtractor;
 import com.google.common.collect.Lists;
@@ -20,16 +21,17 @@ public class LogEntryParser {
     public static final Identification TimeStampIdentification = new Identification("timestamp");
     public static final Identification LoggedThreadIdentification = new Identification("thread");
     public static final Identification LogLevelIdentification = new Identification("level");
-    public static final Identification LoggerNameIdentification = new Identification("logger name");
+    public static final Identification LoggerNameIdentification = new Identification("location");
+    public static final Identification LogMessageIdentifcation = new Identification("message");
 
     public LogEntry parse(String text) {
         Collection<LogPart> bucket = Lists.newArrayList();
         bucket.add(new StringLogPart(DescriptorFor(RawLogIdentification).thatIsNotDisplayed().canNotBeFilteredBy().build(), text));
-        new TimeStampExtractor(DescriptorFor(TimeStampIdentification).thatIsDisplayedAs("timestamp").canNotBeFilteredBy().build(), text).passLogPartTo(bucket);
+        new TimeStampExtractor(DescriptorFor(TimeStampIdentification).thatIsDisplayedAs("timestamp").thatIsNotDisplayed().canNotBeFilteredBy().build(), text).passLogPartTo(bucket);
         new LoggedThreadExtractor(DescriptorFor(LoggedThreadIdentification).thatIsDisplayedAs("thread").isFilterable().build(), text).passLogPartTo(bucket);
         new LogLevelExtractor(DescriptorFor(LogLevelIdentification).thatIsDisplayedAs("level").isFilterable().build(), text).passLogPartTo(bucket);
-        new LogLocationExtractor(DescriptorFor(LoggerNameIdentification).thatIsDisplayedAs("class").isFilterable().build(), text).passLogPartTo(bucket);
-
+        new LogLocationExtractor(DescriptorFor(LoggerNameIdentification).thatIsDisplayedAs("class").canNotBeFilteredBy().build(), text).passLogPartTo(bucket);
+        new LogMessageExtractor(DescriptorFor(LogMessageIdentifcation).thatIsDisplayedAs("message").canNotBeFilteredBy().build(), text).passLogPartTo(bucket);
         return LogEntry.Create(bucket);
     }
 }
