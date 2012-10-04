7bd809d9b12610a004dd2da20704e1988c6834ce
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-10-04 18:12:31 +0200

    Added new Test

--- src/test/java/com/github/signed/log/filter/LogPartFilterModel_Test.java
+++ src/test/java/com/github/signed/log/filter/LogPartFilterModel_Test.java
@@ -1,9 +1,16 @@
 package com.github.signed.log.filter;
 
 import com.github.signed.log.core.Authority;
+import com.github.signed.log.core.LogPart;
 import com.github.signed.log.list.LogModel;
+import lang.ArgumentClosure;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -19,5 +26,21 @@ public class LogPartFilterModel_Test {
         verify(wrapped).describeTo(authority);
     }
 
+    @Test
+    public void passTheElementsAsSelected() throws Exception {
+        LogPart first = mock(LogPart.class);
+        model.matches(first);
 
+        assertThat(theWitheListedParts(), hasItems(first));
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<LogPart> theWitheListedParts() {
+        ArgumentClosure<List<LogPart>> whiteListedLogParts = mock(ArgumentClosure.class);
+        ArgumentCaptor<List> captor = ArgumentCaptor.forClass(List.class);
+        model.provideSelectedThreadsTo(whiteListedLogParts);
+        verify(whiteListedLogParts).excecute(captor.capture());
+
+        return captor.getValue();
+    }
 }
